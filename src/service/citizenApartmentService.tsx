43eb6beda27bde57b652374c5bd3c57843b03017
trunghn2003 <134114519+trunghn2003@users.noreply.github.com>
2024-05-06 14:34:06 +0700

    add service getCitizenApartmentsByApartmentId

--- src/service/citizenApartmentService.tsx
+++ src/service/citizenApartmentService.tsx
@@ -4,3 +4,8 @@ export const getCitizenApartments = async () => {
     const result = await get("CitizenApartment");
     return result;
   };
+export const getCitizenApartmentsByApartmentId = async (id: number) => {
+    const result = await get(`CitizenApartment/CitizensByApartment/${id}`);
+    return result;
+  };
+
